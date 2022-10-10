6b125dd251ef2dba431dfddec2c55f6233484431
Junho Jeon <junhjeon@c7r2s8.42seoul.kr>
2022-10-10 15:47:34 +0900

    init

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seungsle <seungsle@student.42.fr>          +#+  +:+       +#+        */
+/*   By: seungsle <seungsle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 12:17:37 by seungsle          #+#    #+#             */
-/*   Updated: 2022/10/06 12:17:37 by seungsle         ###   ########.fr       */
+/*   Updated: 2022/10/09 17:20:57 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+
+void    print_node(t_token_list *token_list)
+{
+    t_token *now;
+
+    now = token_list->head->next;
+    while (now)
+    {
+        printf("%s dollar:%d\n", now->token, now->in_dollar);
+        now = now->next;
+    }
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	t_data      data;
+	char        *str;
+
+	(void)argc;
+	(void)argv;
+	parse_env(envp, &data);
+	update_shlvl(&data);
+	// printf("%s %s\n", search_env(data.env_list, "SHLVL")->val, search_env(data.env_list, "SHLVL")->key);
+	while (1)
+	{
+		str = readline("minishell$ ");// 컴파일시 -lreadline 추가
+		if (str)
+			printf("%s\n", str);//str을 넘겨서 파싱
+		else
+			break ;
+		add_history(str);
+		parse_token(&data, str);
+		print_node(data.token_list);
+		free(str);
+	}
+}
+//env 저장
+//"", '' 파싱
+// $ 
---
+++ srcs/node_token.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   node_token.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seungsle <seungsle@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 12:29:41 by seungsle          #+#    #+#             */
+/*   Updated: 2022/10/09 23:02:27 by seungsle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../includes/minishell.h"
+
+t_token	*search_token(t_token_list *list, char *token)
+{
+    t_token   *now;
+
+    now = list->head->next;
+    while (now)
+    {
+        if (ft_strncmp(now->token, token, -1) == 0)
+            return (now);
+        now = now->next;
+    }
+    return (0);
+}
+
+int	create_token_list_sub(t_token_list *list)
+{
+	t_token	*head;
+	t_token	*tail;
+
+	list->head = (t_token *)malloc(sizeof(t_token));
+	if (list->head == 0)
+		return (1);
+	list->tail = (t_token *)malloc(sizeof(t_token));
+	if (list->tail == 0)
+		return (1);
+	head = list->head;
+	tail = list->tail;
+	head->token = 0;
+	head->in_dollar = 0;
+	head->around = -1;
+	tail->token = 0;
+	tail->in_dollar = 0;
+	tail->around = -1;
+	head->next = tail;
+	head->prev = 0;
+	tail->next = 0;
+	tail->prev = head;
+	return (0);
+}
+
+void	create_token_list(t_data *data)
+{
+	data->token_list = (t_token_list *)malloc(sizeof(t_token_list));
+	if (data->token_list == 0)
+		exit(1);
+	if (create_token_list_sub(data->token_list))
+		exit(1);
+	data->token_list->count = 0;
+}
+
+
+void	append_token_sub(t_token_list *list, t_token *new_node)
+{
+	list->tail->prev->next = new_node;
+	new_node->prev = list->tail->prev;
+	new_node->next = list->tail;
+	list->tail->prev = new_node;
+	list->count++;
+}
+
+int	append_token(t_token_list *list, t_parse *parse, char *token, size_t size)
+{
+	t_token		*new_node;
+	char		buf;
+	char		*ret;
+
+	if (parse->in_qout)
+	{
+		printf("unclose qout\n");
+		return (0);
+	}
+	ft_strlcpy(&buf, token, size + 1);
+	ret = ft_strdup(&buf);
+	new_node = 0;
+	new_node = create_token(ret, parse);
+	if (new_node == 0)
+		return (1);
+	append_token_sub(list, new_node);
+	return (0);
+}
+
+t_token	*create_token(char *token, t_parse *parse)
+{
+	t_token	*new_node;
+
+	new_node = (t_token *)malloc(sizeof(t_token));
+	if (new_node == 0)
+		return (0);
+	new_node->token = token;
+	new_node->in_dollar = parse->in_dollar;
+	new_node->around = -1;
+	new_node->next = 0;
+	new_node->prev = 0;
+	return (new_node);
+}
