4001b41e6cb08ce6a09248a979558bb72caecd53
aLVINlEE9 <fldh3746@gmail.com>
2022-10-14 15:59:51 +0900

    fixing

--- srcs/parse_token_2.c
+++ srcs/parse_token_2.c
@@ -6,7 +6,7 @@
 /*   By: seungsle <seungsle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 13:27:50 by seungsle          #+#    #+#             */
-/*   Updated: 2022/10/14 15:12:49 by seungsle         ###   ########.fr       */
+/*   Updated: 2022/10/14 15:58:44 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,6 +109,15 @@ void	remove_char_from_idx(char *s, int idx)
 	ft_memmove(&s[idx], &s[idx + 1], ft_strlen(s) - idx);
 }
 
+void	remove_string(char *s, int idx_s, int idx_e)
+{
+	while (idx_s <= idx_e)
+	{
+		remove_char_from_idx(s, idx_e);
+		idx_e--;
+	}
+}
+
 void	qout_remove(t_parse *parse)
 {
 	if (parse->in_qout && parse->idxq_e)
@@ -122,9 +131,86 @@ void	qout_remove(t_parse *parse)
 	}
 }
 
+void	replace_util_sub(t_parse *parse, char *first, char *val, char *last)
+{
+	char	*ret;
+	int		len;
+
+	len = ft_strlen(first) + ft_strlen(val) + ft_strlen(last);
+	ret = (char *)malloc(sizeof(char) * len + 1);
+	if (!ret)
+		return ;
+	ft_strlcpycpy(ret, first, ft_strlen(first) + 1);
+	ft_strlcpycpy(&ret[ft_strlen(first)], val, ft_strlen(val) + 1);
+	ft_strlcpycpy(&ret[ft_strlen(first) + ft_strlen(val)], last, len + 1);
+	parse->s = ret;
+}
+
+void	replace_dollar_options(t_data *data, t_parse *parse, char *buf_start, char *buf_end)
+{
+	char	*val;
+
+	val = 0;
+	if (is_dollar_option(&parse->s[parse->i]) == 1)
+		val = ft_itoa(getpid());
+	else if (is_dollar_option(&parse->s[parse->i]) == 2)
+		val = ft_itoa(data->exit_code);
+	remove_char_from_idx(parse->s, parse->i);
+	replace_util_sub(parse, buf_start, val, buf_end);
+}
+
+void	replace_util(t_data *data, t_parse *parse, int idx, int start)
+{
+	t_env	*env;
+	size_t	buf_env_len;
+	char	*buf_start;
+	char	*buf_env;
+	char	*buf_end;
+
+	buf_env_len = idx - start + 1;
+	buf_start = (char *)malloc(sizeof(char) * start + 1);
+	buf_env = (char *)malloc(sizeof(char) * buf_env_len);
+	buf_end = (char *)malloc(sizeof(char) * ft_strlen(parse->s) - idx + 1);
+	ft_strlcpy(buf_start, parse->s, start);
+	ft_strlcpy(buf_env, &parse->s[start], buf_env_len);
+	ft_strlcpy(buf_end, &parse->s[idx], ft_strlen(parse->s) - idx + 1);
+	if (is_dollar_option(&parse->s[parse->i]))
+		replace_dollar_options(data, parse, buf_start, buf_end);
+	else
+	{
+		env = search_env(data->env_list, buf_env);
+		if (!env)
+        {
+            remove_string(parse->s, parse->i, parse->i + buf_env_len - 1);
+			parse->is_env = FALSE;
+        }
+		else
+        {
+            replace_util_sub(parse, buf_start, env->val, buf_end);
+            parse->is_env = TRUE;
+        }
+	}
+	free(buf_start);
+	free(buf_env);
+	free(buf_end);
+}
+
 void	replace_dollar_to_env(t_data *data, t_parse *parse)
 {
-	
+	int	idx;
+	int	start;
+
+	idx = parse->i + 1;
+	start = idx;
+	if (is_dollar_option(&parse->s[parse->i]))
+		idx++;
+	else
+	{
+		while (!is_space(parse->s[idx]) && !is_quot(parse->s[idx]) && \
+			!is_dollar(parse->s[idx]) && parse->s[idx])
+		idx++;
+	}
+	replace_util(data, parse, idx, start);
 }
 
 int	condition_backslash(t_parse *parse)
@@ -185,8 +271,8 @@ int	condition_append_token(t_parse *parse)
 {
 	if (is_end(parse))
 	{
-		if (!parse->is_env && parse->i - parse->idx == 0)
-			return (0);
+		if (parse->in_dollar && !parse->is_env && parse->i - parse->idx == 0)
+			return (2);
 		else
 			return (1);
 	}
@@ -197,7 +283,7 @@ void	parse_token_sub(t_data *data, t_parse *parse)
 {
 	if (!is_space(parse->s[parse->i]))
 	{
-		while (1)
+		while (parse->s[parse->i])
 		{
 			if (condition_backslash(parse))
 				continue ;
@@ -205,9 +291,9 @@ void	parse_token_sub(t_data *data, t_parse *parse)
 			condition_qout(parse);
 			if (condition_append_token(parse))
 			{
+				parse->i++;
 				append_token(data->token_list, parse, &parse->s[parse->idx], \
 						parse->i - parse->idx);
-				parse->i++;
 				break ;
 			}
 			parse->i++;
